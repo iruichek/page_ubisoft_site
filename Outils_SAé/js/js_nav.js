a4dc82b2a3010681be789bcdc85c95914e79913a
DylanPernot <120225406+DylanPernot@users.noreply.github.com>
2025-10-05 01:37:28 +0200

    test

--- Outils_SAé/js/js_nav.js
+++ Outils_SAé/js/js_nav.js
@@ -1,15 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".nav-link");
+  const currentPage = window.location.pathname.split("/").pop().toLowerCase() || "index.html";
 
-document.addEventListener("DOMContentLoaded", function() {
+  links.forEach(link => {
+    if (link.getAttribute("href").toLowerCase() === currentPage) {
+      link.classList.add("ouverte");   // nouvelle classe pour éviter conflit Bootstrap
+    } else {
+      link.classList.remove("ouverte");
+    }
+  });
 
-const links = document.querySelectorAll('.nav-link');
-const currentPage = window.location.pathname.split("/").pop(); 
-
-links.forEach(link => {
-  if(link.getAttribute('href') === currentPage) {
-    link.classList.add('active');
-  } else {
-    link.classList.remove('active');
+  // Empêche le formulaire de recharger la page
+  const searchForm = document.querySelector('form[role="search"]');
+  if (searchForm) {
+    searchForm.addEventListener('submit', function(e){
+      e.preventDefault();
+    });
   }
-});
-
 });
